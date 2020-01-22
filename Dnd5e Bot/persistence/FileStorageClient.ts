8f0fc391a989a95a07b1cf5a3c248b49af5d9015
Stephen Bayne <sephizor@sephizor.com>
2020-01-22 00:54:55 +0000

    Add file storage client

--- Dnd5e Bot/persistence/FileStorageClient.ts
+++ Dnd5e Bot/persistence/FileStorageClient.ts
@@ -1,16 +1,30 @@
 import IStorageClient from "./IStorageClient";
+import fs from 'fs';
+import path from 'path';
 
 export default class FileStorageClient implements IStorageClient {
 
+    _rootDirectory: string;
+
     constructor(rootDirectory: string) {
+        this._rootDirectory = rootDirectory;
+        try {
+            if(!fs.existsSync(rootDirectory)) {
+                fs.mkdirSync(rootDirectory);
+            }
+        }
+        catch(e) {
+            throw new Error('There was a failure while creating the character due to a bot configuration issue. Please contact the bot owner');
+        }
 
     }
 
     save(json: string, fileName: string): boolean {
-        throw new Error("Method not implemented.");
+        fs.writeFileSync(`${this._rootDirectory}${path.sep}${fileName}`, json);
+        return fs.existsSync(`${this._rootDirectory}${path.sep}${fileName}`);
     }
     fetch(fileName: string): string {
-        throw new Error("Method not implemented.");
+        return fs.readFileSync(`${this._rootDirectory}${path.sep}${fileName}`, 'utf8');
     }
 
 }
